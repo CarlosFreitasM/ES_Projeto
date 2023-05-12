5eff58c5f8286f4f6b4959c7b607031164914d11
Carlos Marques <negphoenix@gmail.com>
2023-05-12 23:19:00 +0100

    Inicialização

---
+++ source/repos/ES_ProjFase2/ES_ProjFase2/Models/ESDatabaseSettings.cs
@@ -0,0 +1,11 @@
+﻿namespace ES_ProjFase2.Models
+{
+    public class ESDatabaseSettings
+    {
+        public string ConnectionString { get; set; } = null!;
+
+        public string DatabaseName { get; set; } = null!;
+
+        public string UserCollectionName { get; set; } = null!;
+    }
+}
---
+++ source/repos/ES_ProjFase2/ES_ProjFase2/Models/User.cs
@@ -0,0 +1,19 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+
+namespace ES_ProjFase2.Models;
+
+public class User
+{
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+    public string Id { get; set; }
+
+    public string Username { get; set; } = null!;
+    public string Password { get; set; } = null!;
+    public string Email { get; set; } = null!;
+    public string Phone { get; set; } = null!;
+    public bool isAdmin { get; set; }
+}
+
